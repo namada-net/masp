8251273e013e04e8eb4f64160df2eded41ddd8fe
Tomáš Zemanovič <tomas@heliax.dev>
2025-07-23 12:03:24 +0100

    cargo fmt

--- masp_primitives/src/transaction/components/transparent/builder.rs
+++ masp_primitives/src/transaction/components/transparent/builder.rs
@@ -194,8 +194,7 @@ impl TransparentAuthorizingContext for Unauthorized {
 #[cfg(feature = "transparent-inputs")]
 impl TransparentAuthorizingContext for Unauthorized {
     fn input_amounts(&self) -> Vec<(AssetType, u64)> {
-        self
-            .inputs
+        self.inputs
             .iter()
             .map(|txin| (txin.coin.asset_type, txin.coin.value))
             .collect()
